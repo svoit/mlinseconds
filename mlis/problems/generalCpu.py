bd5c4b89d598aca47d6263f75f851761ab3ced62
svoit <1726513+svoit@users.noreply.github.com>
2019-05-13 23:33:07 +0200

    Solution for generalCpu.py with avg steps = 6.9
    Average steps = 6.900
    Average time = 0.025/2.000
    Average size = 8471.000/10000.000

--- mlis/problems/generalCpu.py
+++ mlis/problems/generalCpu.py
@@ -9,55 +9,87 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from ..utils import solutionmanager as sm
+from ..utils import gridsearch as gs
 
 class SolutionModel(nn.Module):
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size, output_size, solution):
         super(SolutionModel, self).__init__()
+        self.solution = solution
         self.input_size = input_size
-        sm.SolutionManager.print_hint("Hint[1]: Explore more deep neural networks")
-        self.hidden_size = 10
+        self.hidden_size = solution.hidden_size
+        self.lr = solution.lr
+
         self.linear1 = nn.Linear(input_size, self.hidden_size)
+        self.linear1_2 = nn.Linear(self.hidden_size, self.hidden_size)
+        self.linear1_3 = nn.Linear(self.hidden_size, self.hidden_size)
+        self.linear1_4 = nn.Linear(self.hidden_size, self.hidden_size)
         self.linear2 = nn.Linear(self.hidden_size, output_size)
+
+        self.batch_norm1 = nn.BatchNorm1d(self.hidden_size, track_running_stats=False)
+        self.batch_norm1_2 = nn.BatchNorm1d(self.hidden_size, track_running_stats=False)
+        self.batch_norm1_3 = nn.BatchNorm1d(self.hidden_size, track_running_stats=False)
+        self.batch_norm1_4 = nn.BatchNorm1d(self.hidden_size, track_running_stats=False)
 
     def forward(self, x):
         x = self.linear1(x)
-        x = torch.sigmoid(x)
+        x = F.relu6(x)
+        x = self.batch_norm1(x)
+
+        x = self.linear1_2(x)
+        x = F.relu6(x)
+        x = self.batch_norm1_2(x)
+
+        x = self.linear1_3(x)
+        x = F.relu6(x)
+        x = self.batch_norm1_3(x)
+
+        x = self.linear1_4(x)
+        x = F.relu6(x)
+        x = self.batch_norm1_4(x)
+
         x = self.linear2(x)
-        x = torch.sigmoid(x)
+        x = F.sigmoid(x)
+
         return x
 
     def calc_loss(self, output, target):
-        loss = ((output-target)**2).sum()
+        loss = F.binary_cross_entropy(output, target)
         return loss
 
     def calc_predict(self, output):
         predict = output.round()
         return predict
 
+
 class Solution():
     def __init__(self):
-        self = self
+        self.lr = 0.009
+        self.lr_grid = [0.0001, 0.001, 0.005, 0.01, 0.1, 1]
+
+        self.hidden_size = 50
+        self.hidden_size_grid = [15, 20, 25, 30, 35, 40, 45, 50, 55]
+
+        self.grid_search = gs.GridSearch(self).set_enabled(False)
 
     def create_model(self, input_size, output_size):
-        return SolutionModel(input_size, output_size)
+        return SolutionModel(input_size, output_size, self)
 
     # Return number of steps used
     def train_model(self, model, train_data, train_target, context):
         step = 0
         # Put model in train mode
         model.train()
+        optimizer = optim.Adam(model.parameters(), lr=model.lr)
         while True:
             time_left = context.get_timer().get_time_left()
             # No more time left, stop training
-            if time_left < 0.1:
+            if time_left < 0.1 or (model.solution.grid_search.enabled and step > 10):
                 break
-            optimizer = optim.SGD(model.parameters(), lr=1.0)
             data = train_data
             target = train_target
             # model.parameters()...gradient set to zero
             optimizer.zero_grad()
             # evaluate model => model.forward(data)
-            sm.SolutionManager.print_hint("Hint[2]: Explore other activation functions", step)
             output = model(data)
             # if x < 0.5 predict 0 else predict 1
             predict = model.calc_predict(output)
@@ -65,9 +97,12 @@ class Solution():
             correct = predict.eq(target.view_as(predict)).long().sum().item()
             # Total number of needed predictions
             total = predict.view(-1).size(0)
+            if total == correct:
+                break
             # calculate loss
-            sm.SolutionManager.print_hint("Hint[3]: Explore other loss functions", step)
             loss = model.calc_loss(output, target)
+            if model.solution.grid_search.enabled:
+                self.grid_search.log_step_value('loss', loss.item(), step)
             # calculate deriviative of model.forward() and put it in model.parameters()...gradient
             loss.backward()
             # print progress of the learning
